0d9a87e7c44430a819b16c364e2dd81004965299
jtmb <james.branco@gmail.com>
2024-08-04 16:47:31 -0400

    added config options

--- ansible_provisioning/vars/vars.sh
+++ ansible_provisioning/vars/vars.sh
@@ -16,7 +16,7 @@ source $homedir/ansible_provisioning/wrapper-scripts/vault-auth.sh
 # ANSIBLE VARS
 ANSIBLE_SUDO_PASS=$(vault kv get -field=admin_pwd kv/admin_pass)
 ANSIBLE_SSH_USER=pi
-SSH_PORT=22
+SSH_PORT=2002
 
 # INSTANCE IP's
 retropie=192.168.0.28
--- run-ansible.sh
+++ run-ansible.sh
@@ -4,7 +4,7 @@ source ansible_provisioning/vars/vars.sh
 # Ansible Playbook Consumes VARS
 cd ansible_provisioning && \
      ANSIBLE_CONFIG=./ansible_provisioning/ansible.cfg ansible-playbook -i inventory.ini main.yml \
-    --limit 'all' --skip-tags "none" --tags "all" \
+    --limit 'all' --skip-tags "none" --tags "config" \
     --extra-vars "ssh_port=$SSH_PORT pub_key=$pub_key \
     retropie=$retropie \
     ssh_cert=$HOME/.ssh/id_ed25519 homedir=$homedir \
@@ -12,15 +12,6 @@ cd ansible_provisioning && \
     ssh_user=$ANSIBLE_SSH_USER ansible_sudo_pass=$ANSIBLE_SUDO_PASS "
 
 # TAGS 
-    # containers
-    # swarm 
-    # update_ubuntu
-    # pre_prov
-    # backup_all
-    # backup_mc
+    # config
     # smb 
-    # reboot 
-    # dns 
-    # mc 
-    # ssh 
-    # cloudflare
+    # ssh
