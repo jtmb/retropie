30abb0f0ad15192cac97284e4ab40c4fb2753d9e
jtmb <james.branco@gmail.com>
2024-08-04 17:31:09 -0400

    updated readme

--- ansible_provisioning/vars/vars.sh
+++ ansible_provisioning/vars/vars.sh
@@ -3,13 +3,6 @@
 # #   Vars must be set in order for ansible deploy to work. If you are unsure about what these VARS do, search for them in the project and get familiar.  # #
 #############################################################################################################################################################
 
-
-# GENERAL VARS
-userid=$(whoami)
-homedir=/home/$userid/repos/retropie
-public_ip=$(curl icanhazip.com)
-admin_pub_ip=$(echo ['"'$public_ip/32'"'])
-
 # VAULT AUTH
 source $homedir/ansible_provisioning/wrapper-scripts/vault-auth.sh 
 
