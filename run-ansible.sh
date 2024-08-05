4ee134556338b4dd63d321592fc4d1c019cdd16e
jtmb <james.branco@gmail.com>
2024-08-04 21:45:58 -0400

    updated

--- run-ansible.sh
+++ run-ansible.sh
@@ -4,10 +4,9 @@ source ansible_provisioning/vars/vars.sh
 # Ansible Playbook Consumes VARS
 cd ansible_provisioning && \
      ANSIBLE_CONFIG=./ansible_provisioning/ansible.cfg ansible-playbook -i inventory.ini main.yml \
-    --limit 'all' --skip-tags "none" --tags "alias" \
+    --limit 'all' --skip-tags "none" --tags "all" \
     --extra-vars "ssh_port=$SSH_PORT RETROPIE_IP=$RETROPIE_IP \
     ssh_cert=$HOME/.ssh/id_ed25519 homedir=$homedir \
-    user_id=$userid \
     ssh_user=$ANSIBLE_SSH_USER ansible_sudo_pass=$ANSIBLE_SUDO_PASS "
 
 # TAGS 
