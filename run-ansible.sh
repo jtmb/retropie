f6609f53658830789b0867860375ce62658ef5a3
jtmb <james.branco@gmail.com>
2024-08-04 20:59:07 -0400

    added alias

--- run-ansible.sh
+++ run-ansible.sh
@@ -4,7 +4,7 @@ source ansible_provisioning/vars/vars.sh
 # Ansible Playbook Consumes VARS
 cd ansible_provisioning && \
      ANSIBLE_CONFIG=./ansible_provisioning/ansible.cfg ansible-playbook -i inventory.ini main.yml \
-    --limit 'all' --skip-tags "none" --tags "config" \
+    --limit 'all' --skip-tags "none" --tags "alias" \
     --extra-vars "ssh_port=$SSH_PORT pub_key=$pub_key \
     retropie=$retropie \
     ssh_cert=$HOME/.ssh/id_ed25519 homedir=$homedir \
@@ -15,3 +15,4 @@ cd ansible_provisioning && \
     # config
     # smb 
     # ssh
+    # alias
