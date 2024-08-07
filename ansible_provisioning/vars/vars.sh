4d0816528a501c8d143b005505b6af21bad58865
jtmb <james.branco@gmail.com>
2024-08-07 12:50:58 -0400

    updated

--- ansible_provisioning/vars/vars.sh
+++ ansible_provisioning/vars/vars.sh
@@ -9,7 +9,7 @@ source $homedir/ansible_provisioning/wrapper-scripts/vault-auth.sh
 # ANSIBLE VARS
 ANSIBLE_SUDO_PASS=$(vault kv get -field=admin_pwd kv/admin_pass)
 ANSIBLE_SSH_USER=pi
-SSH_PORT=2002
+SSH_PORT=22
 
 # INSTANCE IP's
 RETROPIE_IP=192.168.0.28
